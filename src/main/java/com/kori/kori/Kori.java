7320ffa2e0497089719f7a415666f69be15ff603
Ali <abenrami06@gmail.com>
2026-01-22 12:14:26 -0600

    kori class update

--- src/main/java/com/kori/kori/Kori.java
+++ src/main/java/com/kori/kori/Kori.java
@@ -2,12 +2,14 @@ package com.kori.kori;
 
 import com.kori.kori.model.KoriState;
 import javafx.animation.Animation;
+import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.image.PixelWriter;
+import javafx.scene.image.PixelReader;
 import javafx.scene.image.WritableImage;
-import javafx.scene.paint.Color;
+import javafx.util.Duration;
+
 
 public class Kori extends ImageView {
 
@@ -16,19 +18,43 @@ public class Kori extends ImageView {
 
     private KoriState state = KoriState.IDLE;
 
-    private Timeline animation = new Timeline();
+    private int keyframe = 0;
+    private int NumberOfFrames = 10;
+    private final Image koriSpriteMap = new Image(String.valueOf(getClass().getResource("assets/kori/KoriAnimationMap.png")), 100, 150, false, true);
+
+    private final Timeline animation;
 
     public Kori() {
-        Image koriSpritMap = new Image("com/kori/kori/assets/icon/favicon.png", 100, 150, false, true);
-        this.setImage(koriSpritMap);
+        resizeImage();
+        animation = new Timeline(new KeyFrame(Duration.seconds(1),
+        e -> {
+            PlayKeyFrame();
+            resizeImage();
+        }
+        ));
+
+        animation.setCycleCount(Animation.INDEFINITE);
+
+        this.updateAnimation();
         this.start();
+    }
+    private void PlayKeyFrame(){
+        this.keyframe = (this.keyframe + 1) % NumberOfFrames;
+    }
+
+    private void resizeImage() {
+        int koriSize = 32;
+        int x = this.keyframe * 32;
+        int y = this.state.ordinal() * 32;
+        PixelReader reader = koriSpriteMap.getPixelReader();
+        this.setImage(new WritableImage(reader, x, y, koriSize, koriSize));
     }
 
     private void updateAnimation() {
-        // animation = new Timeline();
+         resizeImage();
     }
 
-    private void setState(KoriState s) {
+    public void setState(KoriState s) {
         this.state = s;
         this.updateAnimation();
     }
